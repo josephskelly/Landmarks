f92b27dcab87d4f3028c30a26b8e463b089ed103
Joseph Kelly <jskelly84@gmail.com>
2020-03-07 00:56:22 -0800

    Initial commit.
    Add description text. Add custom image view with circle overlay. Add custom map view.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Joseph Kelly on 3/7/20.
+//  Copyright © 2020 BadMotivator. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
