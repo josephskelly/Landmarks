ee924fbe4d9c46f373d4d0b7a5c10c8e1439f795
Joseph Kelly <jskelly84@gmail.com>
2020-03-06 22:06:58 -0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Joseph Kelly on 3/6/20.
+//  Copyright © 2020 BadMotivator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
