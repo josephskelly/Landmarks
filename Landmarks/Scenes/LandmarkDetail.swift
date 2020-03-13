b47739ee6345630dc1a700250005c2410635b99e
Joseph Kelly <jskelly84@gmail.com>
2020-03-13 09:32:57 -0700

    fix: preview loads correctly
    adds environmentObject(UserData()) to preview data

--- Landmarks/Scenes/LandmarkDetail.swift
+++ Landmarks/Scenes/LandmarkDetail.swift
@@ -63,5 +63,6 @@ struct LandmarkDetail: View {
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+            .environmentObject(UserData())
     }
 }
