7c53d162404a95570791d855dea3a2b47fb81e70
Joseph Kelly <jskelly84@gmail.com>
2020-03-14 17:29:19 -0700

    feat: adds BadgeSymbol and RotatedBadgeSymbol

---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Joseph Kelly on 3/14/20.
+//  Copyright © 2020 BadMotivator. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
