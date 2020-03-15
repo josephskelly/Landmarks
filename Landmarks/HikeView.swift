afa1b8ab4b9fd0d745cfd3263a8b9ffd173cacb3
Joseph Kelly <jskelly84@gmail.com>
2020-03-15 02:27:52 -0700

    feat: adds HikeView

---
+++ Landmarks/HikeView.swift
@@ -0,0 +1,21 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Joseph Kelly on 3/14/20.
+//  Copyright © 2020 BadMotivator. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
