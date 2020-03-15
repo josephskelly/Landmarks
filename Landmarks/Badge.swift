bce1c03dc73c7ed210eac028f8e581fc1d3795cb
Joseph Kelly <jskelly84@gmail.com>
2020-03-14 17:45:32 -0700

    feat: adds layered image to create complete Badge view

--- Landmarks/Badge.swift
+++ Landmarks/Badge.swift
@@ -9,54 +9,31 @@
 import SwiftUI
 
 struct Badge: View {
-    var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                var width: CGFloat = min(geometry.size.width, geometry.size.height)
-                let height = width
-                let xScale: CGFloat = 0.832
-                let xOffset: CGFloat = (width * (1.0 - xScale)) / 2.0
-                width *= xScale
-                path.move(
-                    to: CGPoint(
-                        x: xOffset + width * 0.95,
-                        y: height * (0.20 + HexagonParameters.adjustment)
-                    )
-                )
-                HexagonParameters.points.forEach {
-                    path.addLine(
-                        to: .init(
-                            x: xOffset + width * $0.useWidth.0 * $0.xFactors.0,
-                            y: height * $0.useHeight.0 * $0.yFactors.0
-                        )
-                    )
-                    path.addQuadCurve(
-                        to: .init(
-                            x: xOffset + width * $0.useWidth.1 * $0.xFactors.1,
-                            y: height * $0.useHeight.1 * $0.yFactors.1
-                        ),
-                        control: .init(
-                            x: xOffset + width * $0.useWidth.2 * $0.xFactors.2,
-                            y: height * $0.useHeight.2 * $0.yFactors.2
-                        )
-                    )
-                }
-            }
-            .fill(
-                LinearGradient(
-                    gradient: .init(colors: [self.gradientStart, self.gradientEnd]),
-                    startPoint: .init(x: 0.5, y: 0),
-                    endPoint: .init(x: 0.5, y: 0.6)
-                )
+    static let rotationCount = 6
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+             RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount) * 360.0)
             )
-        }
+        }.opacity(0.5)
+       
     }
-    let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
-    let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+    .scaledToFit()
+    }
 }
+
 struct Badge_Previews: PreviewProvider {
     static var previews: some View {
         Badge()
-            .previewLayout(.fixed(width: 100, height: 100))
+        //            .previewLayout(.sizeThatFits)
     }
 }
